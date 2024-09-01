6924e2fe067255abaebea2677a9ec644c92b76e1
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-09-01 21:46:51 +0700

    update `dashboard.blade.php`

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Wealthness Spa') }}
         </h2>
+        <input type="checkbox" value="dark" class="toggle theme-controller" />
     </x-slot>
 
     <div class="mt-6">
